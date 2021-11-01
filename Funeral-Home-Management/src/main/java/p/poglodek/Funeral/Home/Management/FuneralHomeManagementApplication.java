a9622954105df4a8b08206d3eb6d79a09be8fd4c
poglodek <pawelogll@gmail.com>
2021-11-01 14:09:49 +0100

    init

---
+++ Funeral-Home-Management/src/main/java/p/poglodek/Funeral/Home/Management/FuneralHomeManagementApplication.java
@@ -0,0 +1,13 @@
+package p.poglodek.Funeral.Home.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuneralHomeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuneralHomeManagementApplication.class, args);
+	}
+
+}
---
+++ Funeral-Home-Management/src/test/java/p/poglodek/Funeral/Home/Management/FuneralHomeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package p.poglodek.Funeral.Home.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuneralHomeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
