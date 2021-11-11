89ca379bdea3cf66bc35e18edcdeee4c52cbebf4
poglodek <pawelogll@gmail.com>
2021-11-11 22:10:26 +0100

    adding tests

---
+++ Funeral-Home-Management/src/test/java/p/poglodek/Funeral/Home/Management/mappers/flowerMapperTest.java
@@ -0,0 +1,59 @@
+package p.poglodek.Funeral.Home.Management.mappers;
+
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import p.poglodek.Funeral.Home.Management.Database.entity.flowerType;
+import p.poglodek.Funeral.Home.Management.Dto.Flower.FlowerDto;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AllArgsConstructor
+class flowerMapperTest {
+
+
+
+    @Test
+    void mapToDto() {
+        var flowerMapper = new flowerMapper();
+        var model = new flowerType(1l,"tulip", "Nice flower", "Red tulip flower", 12.99, null,null);
+        var dto = new FlowerDto(1l,"tulip", "Nice flower", "Red tulip flower", 12.99);
+        var dtoFromMapper = flowerMapper.mapToDto(model);
+        assertEquals(dto.getName(), dtoFromMapper.getName());
+        assertEquals(dto.getPrice(), dtoFromMapper.getPrice());
+        assertEquals(dto.getDescription(), dtoFromMapper.getDescription());
+        assertEquals(dto.getType(), dtoFromMapper.getType());
+    }
+
+    @Test
+    void mapToModel() {
+
+        var flowerMapper = new flowerMapper();
+        var model = new flowerType(1l,"tulip", "Nice flower", "Red tulip flower", 12.99, null,null);
+        var dto = new FlowerDto(1l,"tulip", "Nice flower", "Red tulip flower", 12.99);
+        var modelFromMapper = flowerMapper.mapToModel(dto);
+        assertEquals(dto.getName(), modelFromMapper.getName());
+        assertEquals(dto.getPrice(), modelFromMapper.getPrice());
+        assertEquals(dto.getDescription(), modelFromMapper.getDescription());
+        assertEquals(dto.getType(), modelFromMapper.getType());
+    }
+
+    @Test
+    void mapListToDto() {
+        var flowerMapper = new flowerMapper();
+        var model = new flowerType(1l,"tulip", "Nice flower", "Red tulip flower", 12.99, null,null);
+        var modelList =new ArrayList<flowerType>();
+        modelList.add(model);
+        var dto = new FlowerDto(1l,"tulip", "Nice flower", "Red tulip flower", 12.99);
+        var dtosFromMapper = flowerMapper.mapListToDto(modelList);
+        for (var dtoFromMapper:dtosFromMapper) {
+            assertEquals(dto.getName(), dtoFromMapper.getName());
+            assertEquals(dto.getPrice(), dtoFromMapper.getPrice());
+            assertEquals(dto.getDescription(), dtoFromMapper.getDescription());
+            assertEquals(dto.getType(), dtoFromMapper.getType());
+        }
+
+
+    }
+}
